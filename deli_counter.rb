c25be3477ac2fdd4d79d4ff0188d358089279461
Willis Aybar <willisaybar@gmail.com>
2020-02-01 22:10:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -61,3 +61,10 @@ def take_a_number(line, new_person)
   puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end
 
+def now_serving(line)
+  if line.length == 0 # could say: "if deli.empty?"
+    puts"There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}." 
+  end
+end
