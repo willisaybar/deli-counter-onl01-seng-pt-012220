8413239ede657db93b81203b0a1cfda1abd670d5
Willis Aybar <willisaybar@gmail.com>
2020-02-01 22:09:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -33,3 +33,18 @@
 #     puts "Currently serving #{katz_deli.shift}."
 #   end
 # end
+
+def line(array) 
+  if array.length >= 1
+    nuarray = []
+    counter = 1 
+    array.each do |name|
+      nuarray.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{nuarray.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
