1cb1197d7d4f7faa17f2bc3efd4caf290ec96c19
Willis Aybar <willisaybar@gmail.com>
2020-02-01 22:10:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -56,3 +56,8 @@ def line_simple(array)
   puts current_line
 end 
 
+def take_a_number(line, new_person)
+  line.push(new_person)
+  puts "Welcome, #{new_person}. You are number #{line.length} in line."
+end
+
