b18c25068c348971a8d578a8483ccdcdd7f0aa17
Willis Aybar <willisaybar@gmail.com>
2020-02-01 22:20:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -65,6 +65,7 @@ def now_serving(line)
   if line.length == 0
     puts"There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line.map}." 
+    puts "Currently serving #{line[0]}." 
+    line.shift
   end
 end
